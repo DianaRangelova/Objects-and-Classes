6317f5c7dbbc5b43efbc4b8eae80fac0d1f0f01e
DianaRangelova <diana_kirilova@mail.bg>
2024-03-27 19:24:27 +0200

    Exercise: Objects and Classes 2

---
+++ Exercise-Objects-and-Classes-2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercise_Objects_and_Classes_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputData = Console.ReadLine().Split(", ");
+
+            string title = inputData[0];
+            string content = inputData[1];
+            string author = inputData[2];
+
+            Article article = new Article(title, content, author);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commandInfo = Console.ReadLine().Split(": ");
+                string command = commandInfo[0];
+                string data = commandInfo[1];
+
+                if (command == "Edit")
+                {
+                    article.Edit(data);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(data);
+                }
+                else if (command == "Rename")
+                {
+                    article.Rename(data);
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+}
