10cbf94134661df8bd713a1e4e18147646c371c8
DianaRangelova <diana_kirilova@mail.bg>
2024-03-27 19:18:28 +0200

    Objects and Classes in C#

---
+++ P01-Songs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.Design;
+
+namespace P01_Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> songList = new List<Song>();
+
+            int numberOfSongs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfSongs; i++) 
+            {
+                string[] inputParams = Console.ReadLine().Split("_");
+                string songListType = inputParams[0];
+                string songName = inputParams[1];
+                string songTime = inputParams[2];
+
+                Song currentSong = new Song();
+                currentSong.TypeList = songListType;
+                currentSong.Name = songName;
+                currentSong.Time = songTime;
+
+                // Option 2: Song currentSong = new Song()
+                // {
+                // TypeList = songListType,
+                // Name = songName,
+                // Time = songTime,
+                // };
+
+                songList.Add(currentSong);
+            }
+
+            string currentTypeList = Console.ReadLine();
+
+            if (currentTypeList == "all")
+            {
+                foreach (Song song in songList)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                List<Song> filteredSongs = songList
+                    .Where(s => s.TypeList == currentTypeList)
+                    .ToList();
+
+                foreach (Song song in filteredSongs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+}
+
+class Song
+{
+
+    public string TypeList { get; set; }
+
+    public string Name { get; set; }
+
+    public string Time { get; set; }
+
+}
