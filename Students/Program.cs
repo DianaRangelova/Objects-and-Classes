1d3902f0e0ec6cf6f772a8286d570ea9a8f69503
DianaRangelova <diana_kirilova@mail.bg>
2024-03-27 19:22:05 +0200

    Exercise: Objects and Classes 1

---
+++ Students/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Students
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Students> student = new List<Students>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                double grade = double.Parse(input[2]);
+
+                Students students = new Students()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Grade = grade
+                };
+
+                student.Add(students);
+            }
+
+            foreach (Students students in student.OrderByDescending(student => student.Grade))
+            {
+                Console.WriteLine($"{students.FirstName} {students.LastName}: {students.Grade:F2}");
+            }
+        }
+    }
+}
